c31ddd75d58607f2bd1d633d639a8c099c24e173
Jack Clary <jackclary@hurc-iii.wireless.davidson.edu>
2018-04-05 14:22:10 -0400

    More Files: MQ-X

--- MyScript(s)/DIGscript.py
+++ MyScript(s)/DIGscript.py
@@ -1,13 +1,39 @@
-import time, math
+import time, math, random
 import matplotlib.pyplot as plt
 
 
+#####Function that Adjusts the X-axis values to relfect time objects#####
+
+def xCreation(xList =[], *args):
+    for i in range(100):
+        xList.append(1)
+    return xList
+
+
+#####Function that Adjusts the Y-axis values to reflect correct Gaseous Values#####
+
+def yCreation(yList=[], *args):
+    for i in range(100):
+        yList.append(3)
+    return yList
+
 
 def main():
-    plt.plot([1,2,3,4])
-    plt.ylabel("Y-axis")
-    print("Hello World.")
-    plt.show(block=True)
-
+    #list declaration
+    xList =[]
+    yList =[]
+    #list manipulation
+    try:
+        finalXList = xCreation(xList)
+        finalYList = yCreation(yList)
+    except:
+        print("x or y creation not operational")
+    try:
+        plt.plot(finalXList, finalYList)
+        plt.ylabel("Gas-axis")
+        plt.xlabel("Time-axis")
+        plt.show(block=True)
+    except:
+        print("matplotlib not operating correctly")
 
 main()
